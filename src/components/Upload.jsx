3cb87937f7ee73a22c4dee69d494c0e586f8d54f
satwik-naik <naiksatwik7@gmail.com>
2024-06-16 01:43:15 +0530

    v-3

--- src/components/Upload.jsx
+++ src/components/Upload.jsx
@@ -3,25 +3,39 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
 const Upload = () => {
-  const [file, setFile] = useState("");
-
-  const onsubmit = () => {};
-
+  const [file, setFile] = useState(null);
+  const email = localStorage.getItem("email");
   const navigate = useNavigate();
+
+  const onsubmitFile = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("email", email);
+    formData.append("file", file);
+
+    try {
+      const result = await axios.post("http://localhost:5000/upload", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+
+      console.log(result);
+      alert("File Uploaded Successfully.");
+    } catch (error) {
+      console.error("Error uploading file:", error);
+      alert("File upload failed. Please try again.");
+    }
+  };
+
   return (
-    <div className="bg-gray-100 min-h-screen flex flex-col items-center  justify-evenly">
+    <div className="bg-gray-100 min-h-screen flex flex-col items-center justify-evenly">
       <h1 className="text-7xl font-bold mb-4">UPLOAD FILE </h1>
-      <form
-        action="http://localhost:5000/upload"
-        method="POST"
-        enctype="multipart/form-data"
-        className="flex"
-        onSubmit={onsubmit}
-      >
-        <div class="flex items-center justify-center w-full">
+      <form className="flex" onSubmit={onsubmitFile}>
+        <div className="flex items-center justify-center w-full">
           <label
-            for="dropzone-file"
-            class="flex flex-col items-center justify-center w-[80vw] h-[50vh] border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+            htmlFor="dropzone-file"
+            className="flex flex-col items-center justify-center w-[80vw] h-[50vh] border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
           >
             <div className="flex flex-col items-center justify-center pt-5 pb-6">
               <svg
@@ -33,14 +47,14 @@ const Upload = () => {
               >
                 <path
                   stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
                   d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
                 />
               </svg>
               <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
-                <span class="font-semibold">Click to upload</span>
+                <span className="font-semibold">Click to upload</span>
               </p>
               <p className="text-xs text-gray-500 dark:text-gray-400">
                 SVG, PNG, JPG or GIF (MAX. 800x400px)
@@ -51,14 +65,14 @@ const Upload = () => {
               type="file"
               name="ftp_files"
               className="hidden"
+              multiple
+              onChange={(e) => setFile(e.target.files[0])}
             />
           </label>
         </div>
         <button
+          type="submit"
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-5 px-10 rounded"
-          onClick={() => {
-            alert("File Uploaded Successfully..");
-          }}
         >
           Upload File
         </button>
